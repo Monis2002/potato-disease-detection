bf5747b44fb48a81fe6851c162ba807f41e771d9
Monis2002 <monismomin123@gmail.com>
2024-01-31 20:26:03 +0530

    50 epoch model is added

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def predict(model, img):
     confidence = round(100 * (np.max(predictions[0])), 2)
     return predicted_class, confidence
 
-model=load_model('1')
+model=load_model('model_50_epochs')
 
 def set_bg_hack(main_bg):
    '''
