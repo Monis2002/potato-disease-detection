fdb0a4992e2a7729ef4a247f240b19e49d583ead
Monis2002 <monismomin123@gmail.com>
2024-01-31 12:34:00 +0530

    MOdel is added

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ import streamlit as st
 import os
 from tensorflow.keras.preprocessing import image
 import tensorflow as tf
-from utils import predict
 import joblib as jb
 import base64
+import numpy as np
+
 
 
 # ---------------Requied Data-----------------
+
+def predict(model, img):
+    class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+
+    img_array = tf.keras.preprocessing.image.img_to_array(img.numpy())
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
 model=jb.load('saved_model_50')
 
 def set_bg_hack(main_bg):
