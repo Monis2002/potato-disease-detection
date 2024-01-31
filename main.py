859b6935c88acbab55241bf443090f8881ab598a
Monis2002 <monismomin123@gmail.com>
2024-01-31 08:58:56 +0530

    all project is done

--- main.py
+++ main.py
@@ -1,11 +1,99 @@
 import streamlit as st
+import os
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+from utils import predict
+import joblib as jb
+import base64
 
 
-st.write("""
-# Hi! I am Monis
-Nice to meet you..
+# ---------------Requied Data-----------------
+model=jb.load('saved_model_50')
 
-- I am muin, i am fool
-""")
+def set_bg_hack(main_bg):
+   '''
+   A function to unpack an image from root folder and set as bg.
+
+   Returns
+   -------
+   The background.
+   '''
+   # set bg name
+   main_bg_ext = "png"
+
+   st.markdown(
+      f"""
+        <style>
+        .stApp {{
+            background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+            background-size: cover
+           
+        }}
+        </style>
+        """,
+      unsafe_allow_html=True
+   )
+set_bg_hack('potato.jpg')
+
+
+
+page_bg="""
+<style>
+
+    [data-testid="block-container"]{
+        background-color: #EEEDEB;
+        opacity: 0.85;
+    }
+    
+    [data-testid="stHeader"]{
+        background-color:rgba(0,0,0,0)
+    }
+</style>
+"""
+st.markdown(page_bg,unsafe_allow_html=True)
+
+
+
+
+
+def save_file(img):
+    try:
+        with open(os.path.join('uploaded',img.name),'wb') as f:
+            f.write(img.getbuffer())
+        return 1
+    except:
+        return 0
+
+# Web Devlopment
+st.markdown(f'<h1 style="color:black;font-size:64px;">{"Potato Prediction"}</h1>', unsafe_allow_html=True)
+
+input_type=st.selectbox('Type',['Upload from Gallery','Upload by Cammer'])
+
+if input_type=='Upload by Cammer':
+    uploaded_img = st.camera_input("Take a picture")
+
+else:
+    uploaded_img = st.file_uploader('Upload Image')
+
+
+
+if uploaded_img is not None:
+    st.write('# Image Uploaded')
+
+    if save_file(uploaded_img):
+
+        img = image.load_img(os.path.join('uploaded',uploaded_img.name), target_size=(256, 256))
+        st.image(img)
+        img_array = image.img_to_array(img)
+        img_tf = tf.convert_to_tensor(img_array)
+        class_,confidence=predict(model,img_tf)
+
+        st.write("## Potato Type : ",class_)
+        st.write("## Confidence : ", confidence)
+    else:
+        st.write('Error')
+
+
+
 
 
